06ead82f123de6202f12e51373a28350fb621c45
kminkjan <kminkjan@gmail.com>
2014-12-02 13:48:36 +0100

    Lets go art

--- src/datastructuur/RSHeap.java
+++ src/datastructuur/RSHeap.java
@@ -1,39 +1,81 @@
 package datastructuur;
 
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * The RSHeap class represents a datastructure
+ * 
  * @author Kris
  *
  */
 public class RSHeap {
-	
-	private File file;
-	private List<Integer> heap;
-	private int heapSize, deadSize;
 
-	public RSHeap(File file, int size) {
-		this.heap = new int[size];
-		for (int i = 0; i < heap.length; i++) {
-			// Read int from file
+	private RSHeap leftNode, rightNode, parent;
+	private int value;
+
+	public RSHeap(int value, RSHeap parent) {
+		this.value = value;
+		this.parent = parent;
+	}
+
+	public void insert(int item) {
+		if (leftNode == null) {
+			leftNode = new RSHeap(item, this);
+			leftNode.bubbleUp();
+		} else if (rightNode == null) {
+			rightNode = new RSHeap(item, this);
+			rightNode.bubbleUp();
+		} else if (rightNode.size() < leftNode.size()) {
+			rightNode.insert(item);
+		} else {
+			leftNode.insert(item);
 		}
-		buildHeap(heap);
+	}
+
+	public int swap(int newValue) {
+		int temp = this.value;
+		this.value = newValue;
+		return temp;
+	}
+
+	public void bubbleUp() {
+		if (parent != null && parent.value > this.value) {
+			this.value = parent.swap(this.value);
+			parent.bubbleUp();
+		}
+	}
+
+	public void buildHeap() {
+		if (isLeaf()) {
+			bubbleUp();
+		} else {
+			if (leftNode != null) {
+				leftNode.buildHeap();
+			}
+			if (rightNode != null) {
+				rightNode.buildHeap();
+			}
+		}
 	}
 	
-	private void buildHeap(int[] heap) {
-		int min = heap[0];
-		for (int i = 0; i < heap.length; i++) {
-			
-		}
+	public int getRoot() {
+		return -1;
 	}
-	
-	private void createRun() {
-		while(heapSize != 0) {
-			
+
+	public boolean isLeaf() {
+		if (leftNode == null && rightNode == null) {
+			return true;
 		}
+		return false;
 	}
+
+	public int size() {
+		if (rightNode == null && leftNode == null) {
+			return 1;
+		} else if (rightNode == null) {
+			return leftNode.size() + 1;
+		} else if (leftNode == null) {
+			return rightNode.size() + 1;
+		}
+		return leftNode.size() + rightNode.size() + 1;
+	}
+
 }
-
