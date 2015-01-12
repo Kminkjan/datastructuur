2d2a0cef526ca1f119294b2a90f1d2426591a2e0
kminkjan <kminkjan@gmail.com>
2015-01-12 15:08:19 +0100

    asdsa

--- Minkjan_Uncu_ConcOpdr4.0/src/main/Main.java
+++ Minkjan_Uncu_ConcOpdr4.0/src/main/Main.java
@@ -5,6 +5,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Lets go Akka");
 		System.out.println("DOEWNLOADET");
+		System.out.println("POP THE HOOD");
 	}
 
 }
