d24d1cbe8f0cb425014a6ec069b307e25b23fc20
schaap91 <artemisuncu@gmail.com>
2015-01-12 15:24:39 +0100

    ;

--- Minkjan_Uncu_ConcOpdr4.0/src/main/Main.java
+++ Minkjan_Uncu_ConcOpdr4.0/src/main/Main.java
@@ -3,21 +3,12 @@ package main;
 public class Main {
 
 	public static void main(String[] args) {
-<<<<<<< HEAD
-		System.out.println("Lets go Akka");
 
-		System.out.println("Ik loop, ik liep, ik heb geliept");
-
-		System.out.println("DOEWNLOADET");
-
-		System.out.println("POP THE HOOD");
-
-=======
 		System.out.println("Lets go Akka");
 		System.out.println("Ik loop, ik liep, ik heb geliept");
 		System.out.println("DOEWNLOADET");
 		System.out.println("POP THE HOOD");
->>>>>>> f215274f7f3b6b2486bf23647c5675b2c29821ed
+
 	}
 
 }
