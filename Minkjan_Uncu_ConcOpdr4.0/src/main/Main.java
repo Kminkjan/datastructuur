0580e6183d6b32b0fc88ea0f9e57fda02cfc5876
schaap91 <artemisuncu@gmail.com>
2015-01-12 15:11:16 +0100

    sdaads

--- Minkjan_Uncu_ConcOpdr4.0/src/main/Main.java
+++ Minkjan_Uncu_ConcOpdr4.0/src/main/Main.java
@@ -4,15 +4,13 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Lets go Akka");
-<<<<<<< HEAD
+
 		System.out.println("Ik loop, ik liep, ik heb geliept");
-=======
+
 		System.out.println("DOEWNLOADET");
-<<<<<<< HEAD
->>>>>>> fe4417081c658a11762d90202eecb650c79c33fd
-=======
+
 		System.out.println("POP THE HOOD");
->>>>>>> 2d2a0cef526ca1f119294b2a90f1d2426591a2e0
+
 	}
 
 }
